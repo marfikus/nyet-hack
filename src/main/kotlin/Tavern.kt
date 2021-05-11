beb8320e350a8fad6157080c0e005133360bacaa
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-05-11 17:27:14 +0500

    After ch8

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -1,9 +1,33 @@
+import kotlin.math.roundToInt
 
 const val TAVERN_NAME = "Taernyl's Folly"
+
+var playerGold = 10
+var playerSilver = 10
 
 fun main(args: Array<String>) {
 //    placeOrder("shandy, Dragon's Breath, 5.91")
     placeOrder("elixir, Shirleys's Temple, 4.12")
+}
+
+fun performPurcase(price: Double) {
+    displayBalance()
+    val totalPurse = playerGold + (playerSilver / 100.0)
+    println("Total purse: $totalPurse")
+    println("Purchasing item for $price")
+
+    val remainingBalance = totalPurse - price
+    println("Remaining balance: ${"%.2f".format(remainingBalance)}")
+
+    val remainingGold = remainingBalance.toInt()
+    val remainingSilver = (remainingBalance % 1 * 100).roundToInt()
+    playerGold = remainingGold
+    playerSilver = remainingSilver
+    displayBalance()
+}
+
+private fun displayBalance() {
+    println("Player's purse balance: Gold: $playerGold, Silver: $playerSilver")
 }
 
 private fun placeOrder(menuData: String) {
@@ -13,6 +37,8 @@ private fun placeOrder(menuData: String) {
 
     val (type, name, price) = menuData.split(", ")
     println("Madrigal buys a $name ($type) for $price")
+
+    performPurcase(price.toDouble())
 
     val phrase = if (name == "Dragon's Breath") {
         "Madrigal exclaims: ${toDragonSpeak("Ah, delicious $name!")}"
