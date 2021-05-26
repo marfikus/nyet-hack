d6c096abe4e33676cfe1a904ec400e41389da99b
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-05-26 17:35:41 +0500

    After ch13

--- src/main/kotlin/com/bnr/nyethack/Game.kt
+++ src/main/kotlin/com/bnr/nyethack/Game.kt
@@ -1,6 +1,7 @@
 package com.bnr.nyethack
 fun main(args: Array<String>) {
-    val player = Player()
+//    val player = Player("Madrigal", 100, true, false)
+    val player = Player("Madrigal")
     printPlayerStatus(player)
     player.castFireball()
 }
--- src/main/kotlin/com/bnr/nyethack/Player.kt
+++ src/main/kotlin/com/bnr/nyethack/Player.kt
@@ -1,28 +1,46 @@
 package com.bnr.nyethack
 
-class Player {
-    var name = "madrigal"
-        get() = field.capitalize()
+import java.io.File
+
+class Player(_name: String,
+             var healthPoints: Int = 100,
+             val isBlessed: Boolean,
+             private val isImmortal: Boolean) {
+    var name = _name.trim()
+        get() = "${field.capitalize()} from $hometown"
         private set(value) {
             field = value.trim()
         }
+    val hometown by lazy { selectHometown() }
 
-    var playerHealth = 99
-    val isBlessed = true
-    private val isImmortal = false
+    init {
+        require(healthPoints > 0, { "healthPoints must be greater than zero." })
+        require(name.isNotBlank(), { "Player must have a name." })
+    }
+    constructor(name: String) : this(name,
+        healthPoints = 100,
+        isBlessed = true,
+        isImmortal = false) {
+        if (name.toLowerCase() == "kar") healthPoints = 40
+    }
+
+    private fun selectHometown() = File("/home/alex/IdeaProjects/NyetHack/src/data/towns.txt")
+        .readText()
+        .split("\n")
+        .random()
 
     fun castFireball(numFireballs: Int = 2) =
         println("A glass of Fireball springs into existence. (x$numFireballs)")
 
     fun auraColor(
     ): String {
-        val auraVisible = isBlessed && playerHealth > 50 || isImmortal
+        val auraVisible = isBlessed && healthPoints > 50 || isImmortal
         val auraColor = if (auraVisible) "GREEN" else "NONE"
         return auraColor
     }
 
     fun formatHealthStatus() =
-        when (playerHealth) {
+        when (healthPoints) {
             100 -> "is in excellent condition"
             in 90..99 -> "has a few scratches"
             in 75..89 -> if (isBlessed) {
